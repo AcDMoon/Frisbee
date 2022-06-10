44cadc6a74c7e00ee677caa5d30c0b69bf7b24cf
test <densisssss@mail.ru>
2022-06-10 14:17:55 +0700

    US-(ALL) - index.php - Reporting all if domain is local

--- public/index.php
+++ public/index.php
@@ -2,16 +2,23 @@
 
 use Frisbee\core\Router;
 
+$GLOBALS['base_dir'] = __DIR__ . '/../application/';
+
+$domain = require $GLOBALS['base_dir'] . 'config/validDomain.php';
+if ('frisbee' === $domain['localDomain']) {
+    $errorMode = 1;
+}
+
 error_reporting(E_ALL);
-ini_set("display_errors", 1);
+ini_set("display_errors", $errorMode);
 
 spl_autoload_register(function ($class) {
     $prefix = 'Frisbee\\';
-    $GLOBALS['base_dir'] = __DIR__ . '/../application/';
     $len = strlen($prefix);
     if (strncmp($prefix, $class, $len) !== 0) {
         return;
     }
+    $baseDIr = __DIR__ . '/../application/';
     $relative_class = substr($class, $len);
     $file = $GLOBALS['base_dir'] . str_replace('\\', '/', $relative_class) . '.php';
     if (file_exists($file)) {
