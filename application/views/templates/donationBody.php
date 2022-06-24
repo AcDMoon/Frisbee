d3a89b5267e4597b2624ca4f01dcebe3bf8e5690
test <densisssss@mail.ru>
2022-06-24 14:40:04 +0700

    DT-(1) - Controller, View ()  - Redo the controllers and the navbar info page view into one controller and view

--- application/views/templates/donationBody.php
+++
@@ -1,9 +0,0 @@
-<?=$navbar?>
-
-<main class="container my-5">
-    <h1>Donation</h1>
-    <p class="fs-3">If you want to support the project and keep the creator of the site from starving to death, you can donate some money to this card:</p>
-    <p class="fs-3"> **** **** **** ****</p>
-</main>
-
-
